c32941ea5ba5cbbe7618b999d67d520884b6ce1f
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-05-28 15:39:47 +0200

    - Fix IInput interface

--- lib/raylibCpp/src/Input/IInput.hpp
+++ lib/raylibCpp/src/Input/IInput.hpp
@@ -15,16 +15,17 @@ namespace raylib
 {
     class IInput
     {
-        virtual ~IInput() = default;
-        virtual bool isKeyPressed(KeyBoard key) const = 0;
-        virtual bool isKeyReleased(KeyBoard key) const = 0;
-        virtual MyVector2 getMousePos(void) const = 0;
-        virtual bool isMouseLeftPressed(void) const = 0;
-        virtual bool isMouseRightPressed(void) const = 0;
-        virtual bool isMouseMiddlePressed(void) const = 0;
-        virtual bool isMouseMoved(void) = 0;
-        virtual bool MouseIsOver(MyVector2 pos, MyVector2 size) const = 0;
-        virtual bool MouseIsOverClicked(MyVector2 pos, MyVector2 size) const = 0;
+        public:
+            virtual ~IInput() = default;
+            virtual bool isKeyPressed(KeyBoard key) const = 0;
+            virtual bool isKeyReleased(KeyBoard key) const = 0;
+            virtual MyVector2 getMousePos(void) const = 0;
+            virtual bool isMouseLeftPressed(void) const = 0;
+            virtual bool isMouseRightPressed(void) const = 0;
+            virtual bool isMouseMiddlePressed(void) const = 0;
+            virtual bool isMouseMoved(void) = 0;
+            virtual bool MouseIsOver(MyVector2 pos, MyVector2 size) const = 0;
+            virtual bool MouseIsOverClicked(MyVector2 pos, MyVector2 size) const = 0;
     };
 };
 
