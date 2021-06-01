b66c7d86f0a89ee9a9752b1feccd5c3559d41854
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-06-01 18:33:22 +0200

    Add SaveManager main methods

--- lib/gameEngine/src/SaveManager/SaveManager.cpp
+++ lib/gameEngine/src/SaveManager/SaveManager.cpp
@@ -5,4 +5,39 @@
  * SaveManager.cpp - Created: 01/06/2021
  */
 
-#include "SaveManager/SaveManager.hpp"
+#include "SaveManager.hpp"
+
+SaveManager::~SaveManager()
+{
+    for (auto &file : this->_files)
+        file.second.close();
+}
+
+void SaveManager::closeFile(const string filepath)
+{
+    this->_files[filepath].close();
+    this->_files.erase(filepath);
+}
+
+void SaveManager::write(const string filepath, const void *value, size_t size)
+{
+    ofstream &file = this->_getFile(filepath);
+
+    file.write((const char *) value, size);
+}
+
+void SaveManager::write(const string filepath, const string value)
+{
+    ofstream &file = this->_getFile(filepath);
+
+    file.write(value.c_str(), value.length());
+}
+
+ofstream &SaveManager::_getFile(const string &filepath)
+{
+    if (this->_files.find(filepath) != this->_files.end())
+        return this->_files[filepath];
+
+    this->_files[filepath] = ofstream(filepath);
+    return this->_files[filepath];
+}
--- lib/gameEngine/src/SaveManager/SaveManager.hpp
+++ lib/gameEngine/src/SaveManager/SaveManager.hpp
@@ -8,10 +8,68 @@
 #ifndef SAVE_MANAGER_HPP
 #define SAVE_MANAGER_HPP
 
+#include <vector>
+#include <array>
+#include <memory>
+#include <fstream>
+#include <unordered_map>
+#include "GameEngine.hpp"
+
+using namespace Engine;
+
 namespace Engine
 {
-    template <typename T> class SaveManager {
-        write(const T &value);
+    class SaveManager {
+      public:
+        SaveManager() = default;
+        ~SaveManager();
+
+        void closeFile(const string filepath);
+
+        template <typename T> void write(const string filepath, const T &value)
+        {
+            ofstream &file = this->_getFile(filepath);
+
+            file.write((char *) &value, sizeof(T));
+        }
+
+        template <typename T> void write(const string filepath, const std::vector<T> &value)
+        {
+            ofstream &file = this->_getFile(filepath);
+
+            for (const auto &elem : value)
+                file.write((char *) &elem, sizeof(T));
+        }
+
+        template <typename T, size_t N> void write(const string filepath, const std::array<T, N> &value)
+        {
+            ofstream &file = this->_getFile(filepath);
+
+            for (const auto &elem : value)
+                file.write((char *) &elem, sizeof(T));
+        }
+
+        template <typename T> void write(const string filepath, const std::unique_ptr<T> &value)
+        {
+            ofstream &file = this->_getFile(filepath);
+
+            file.write(value.get(), sizeof(T));
+        }
+
+        template <typename T> void write(const string filepath, const std::shared_ptr<T> &value)
+        {
+            ofstream &file = this->_getFile(filepath);
+
+            file.write(value.get(), sizeof(T));
+        }
+
+        void write(const string filepath, const void *value, size_t size);
+        void write(const string filepath, const string value);
+
+      private:
+        [[nodiscard]] ofstream &_getFile(const string &filepath);
+
+        std::unordered_map<string, ofstream> _files;
     };
 } // namespace Engine
 
