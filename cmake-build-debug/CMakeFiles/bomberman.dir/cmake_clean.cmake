5874a0bd9eb66ab30d82c41652f85c53daf1d4d0
arthur.jourdan <arthur.jourdan@epitech.eu>
2021-06-01 11:52:45 +0200

    feat(CMakeLists.txt): add compilation of subdirectories for libs and change cpp version, from 11 to 17

---
+++ cmake-build-debug/CMakeFiles/bomberman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bomberman"
+  "../bomberman.pdb"
+  "CMakeFiles/bomberman.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bomberman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/gameEngine/cmake-build-debug/CMakeFiles/gameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameEngine.dir/src/AbstractSystem/AbstractSystem.cpp.o"
+  "CMakeFiles/gameEngine.dir/src/Component/Component.cpp.o"
+  "CMakeFiles/gameEngine.dir/src/EntityContainer/EntityRegister.cpp.o"
+  "CMakeFiles/gameEngine.dir/src/SystemManager/SystemManager.cpp.o"
+  "CMakeFiles/gameEngine.dir/src/main.cpp.o"
+  "CMakeFiles/gameEngine.dir/src/system/PhysicsSystem/PhysicsSystem.cpp.o"
+  "gameEngine"
+  "gameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
