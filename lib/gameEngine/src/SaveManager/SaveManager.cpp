ecf3dd41c7d3149770def4a63f85d60a48ebcdd3
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-06-01 16:35:13 +0200

    Add default SaveManager class

---
+++ lib/gameEngine/src/SaveManager/SaveManager.cpp
@@ -0,0 +1,8 @@
+/*
+ * EPITECH PROJECT, 2021
+ * SaveManager
+ * File description:
+ * SaveManager.cpp - Created: 01/06/2021
+ */
+
+#include "SaveManager/SaveManager.hpp"
---
+++ lib/gameEngine/src/SaveManager/SaveManager.hpp
@@ -0,0 +1,18 @@
+/*
+ * EPITECH PROJECT, 2021
+ * SaveManager
+ * File description:
+ * SaveManager.hpp - Created: 01/06/2021
+ */
+
+#ifndef SAVE_MANAGER_HPP
+#define SAVE_MANAGER_HPP
+
+namespace Engine
+{
+    template <typename T> class SaveManager {
+        write(const T &value);
+    };
+} // namespace Engine
+
+#endif
