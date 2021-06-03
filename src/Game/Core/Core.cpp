07743b0b4b3bc3d5323375812e83424f405994a8
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-06-03 19:38:12 +0200

    - Add configuration file in Core

--- src/Game/Core/Core.cpp
+++ src/Game/Core/Core.cpp
@@ -11,11 +11,7 @@ using namespace Game;
 
 static const std::string CONFIG_FILE = "bomberman.config";
 
-static const MyVector3 CAM_POSITION = {300, 0, 100};
-static const MyVector3 CAM_TARGET = {0, 0, 0};
-static const MyVector3 CAM_UP = {0, 1, 0};
-
-std::unique_ptr<raylib::Camera> Core::camera = std::make_unique<raylib::Camera>(CAM_POSITION, CAM_TARGET, CAM_UP);
+std::unique_ptr<raylib::Camera> Core::camera = nullptr;
 
 Core::Core()
     : _settings(CONFIG_FILE), _window(_settings.getMyVector2("WIN_SIZE"), _settings.getString("WIN_TITLE"), static_cast<RColor>(_settings.getInt("WIN_BACK"))),
@@ -29,6 +25,7 @@ Core::Core()
     _systemManager.createSystem<System::Render2DSystem>(_entityManager);
     // SCENES - CREATION
     _sceneManager.createScene<DebugScene>(_systemManager, _entityManager);
+    this->camera = std::make_unique<raylib::Camera>(_settings.getMyVector3("CAM_POSITION"), _settings.getMyVector3("CAM_TARGET"), _settings.getMyVector3("CAM_UP"));
 }
 
 Core::~Core()
