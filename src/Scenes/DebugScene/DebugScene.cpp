64d33768fbeaa1dacbce3e5536eb0ec7766c788b
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-06-03 14:34:04 +0200

    fix(src/Scene/DebugScene) declare MyVector as class and not struct cast

--- src/Scenes/DebugScene/DebugScene.cpp
+++ src/Scenes/DebugScene/DebugScene.cpp
@@ -17,14 +17,14 @@ DebugScene::DebugScene(Engine::SystemManager &systemManager, Engine::EntityManag
     /// ENTITIES - CREATION
     auto rect = this->createLocalEntity();
     this->_entityManager.addComponent<Component::Render2D>(
-        rect, std::make_shared<raylib::Rectangle>((raylib::MyVector2){10, 10}, (raylib::MyVector2){20, 20}));
+        rect, std::make_shared<raylib::Rectangle>(raylib::MyVector2(10, 10), raylib::MyVector2(20, 20)));
 
     auto block = this->createLocalEntity();
     raylib::MyVector3 blockPos(0, 20, 0);
     this->_entityManager.addComponent<Component::Render3D>(
-        block, std::make_shared<raylib::Cuboid>(nullptr, blockPos, (raylib::MyVector3){50, 50, 50}, raylib::RColor::RRED));
+        block, std::make_shared<raylib::Cuboid>(nullptr, blockPos, raylib::MyVector3(50, 50, 50), raylib::RColor::RRED));
     this->_entityManager.addComponent<Component::Hitbox>(
-        block, blockPos, (raylib::MyVector3){50, 50, 50}, [](const Engine::Entity &fromEntity, const Engine::Entity &toEntity) {
+        block, blockPos, raylib::MyVector3(50, 50, 50), [](const Engine::Entity &fromEntity, const Engine::Entity &toEntity) {
             auto cubeComponent = Game::Core::entityManager.getComponent<Component::Render3D>(fromEntity);
             auto cube = static_cast<raylib::Cuboid *>(cubeComponent.modele.get());
 
@@ -34,10 +34,10 @@ DebugScene::DebugScene(Engine::SystemManager &systemManager, Engine::EntityManag
     auto moveableEntity = this->createLocalEntity();
     raylib::MyVector3 moveableEntityPos(20, 20, 0);
     this->_entityManager.addComponent<Component::Render3D>(moveableEntity,
-        std::make_shared<raylib::Cuboid>(nullptr, moveableEntityPos, (raylib::MyVector3){50, 50, 50}, raylib::RColor::RMAGENTA));
+        std::make_shared<raylib::Cuboid>(nullptr, moveableEntityPos, raylib::MyVector3(50, 50, 50), raylib::RColor::RMAGENTA));
     this->_entityManager.addComponent<Engine::Position>(moveableEntity, 100, 20);
     this->_entityManager.addComponent<Engine::Velocity>(moveableEntity, 20, 0);
-    this->_entityManager.addComponent<Component::Hitbox>(moveableEntity, moveableEntityPos, (raylib::MyVector3){50, 50, 50},
+    this->_entityManager.addComponent<Component::Hitbox>(moveableEntity, moveableEntityPos, raylib::MyVector3(50, 50, 50),
         [](const Engine::Entity &fromEntity, const Engine::Entity &toEntity) {
         });
 }
