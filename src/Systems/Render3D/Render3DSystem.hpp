1d229bbe666fea3709c0697fe6b031c5e0e5e54e
RACAUD Simon <simon.racaud@epitech.eu>
2021-06-03 10:06:24 +0200

    feat(game-core/systems): Render3DSystem

---
+++ src/Systems/Render3D/Render3DSystem.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2021
+** gameEngine
+** File description:
+** 02/06/2021 Render3DSystem.hpp.h
+*/
+
+#ifndef RENDER3DSYSTEM_HPP
+#define RENDER3DSYSTEM_HPP
+
+#include "GameEngine.hpp"
+#include "Components/Render3D/Render3D.hpp"
+#include "Game/Core/Core.hpp"
+
+namespace System
+{
+    class Render3DSystem : public Engine::AbstractSystem {
+      public:
+        Render3DSystem(Engine::EntityManager &entityManager);
+
+        void update();
+    };
+}
+
+#endif // RENDER3DSYSTEM_HPP
