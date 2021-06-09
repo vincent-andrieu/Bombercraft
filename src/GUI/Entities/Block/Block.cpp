650d70093002ead9b3c6e10c688afdfbf97b1a5f
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-06-09 15:39:48 +0200

    - Refacto Block

--- src/GUI/Entities/Block/Block.cpp
+++ src/GUI/Entities/Block/Block.cpp
@@ -10,23 +10,18 @@
 using namespace Entities;
 
 Block::Block(Engine::EntityPack &entities, const std::string &name, raylib::MyVector3 pos, BlockType type)
-: _type(type), _name(name), _entity(entities.createEntity(name))
+: _type(type), _name(name), _entity(entities.createEntity(name)), _pos(pos)
 {
     raylib::MyVector3 size = Game::CoreData::settings->getMyVector3("DEFAULT_BLOCK_SIZE");
     std::shared_ptr<raylib::Model> model = this->getModel(pos, type);
 
     Game::CoreData::entityManager->addComponent<Engine::Position>(this->_entity, pos.a, pos.b, pos.c);
     Game::CoreData::entityManager->addComponent<Component::Render3D>(this->_entity, model);
-    if (type != BlockType::BLOCK_FLOOR) {
-        Game::CoreData::entityManager->addComponent<Component::Hitbox>(this->_entity, pos, size,
-            []([[maybe_unused]] const Engine::Entity &fromEntity, [[maybe_unused]] const Engine::Entity &toEntity) {
-        });
-    }
+    this->internalFactory(type, pos, size);
 }
 
 Block::~Block()
 {
-
 }
 
 std::shared_ptr<raylib::Model> Block::getModel(raylib::MyVector3 pos, BlockType type) const
@@ -35,14 +30,60 @@ std::shared_ptr<raylib::Model> Block::getModel(raylib::MyVector3 pos, BlockType 
     std::string texturePath;
     std::string typeInStr;
 
-    if (type == BlockType::BLOCK_FLOOR)
-        typeInStr = "FLOOR";
-    else if (type == BlockType::BLOCK_SOFT)
-        typeInStr = "SOFT";
-    else if (type == BlockType::BLOCK_HARD)
-        typeInStr = "HARD";
-    else
-        throw std::invalid_argument("Not define in enum");
+    switch (type)
+    {
+        case BlockType::BLOCK_SOFT: typeInStr = "SOFT"; break;
+        case BlockType::BLOCK_HARD: typeInStr = "HARD"; break;
+        case BlockType::BLOCK_BOMB: typeInStr = "BOMB"; break;
+        case BlockType::BLOCK_BLAST: typeInStr = "BLAST"; break;
+        case BlockType::BLOCK_FLOOR: typeInStr = "FLOOR"; break;
+        default: typeInStr = "DEFAULT"; break;
+    }
     texturePath = Game::CoreData::settings->getString("BLOCK_" + typeInStr + "_TEXTURE");
     return std::make_shared<raylib::Model>(texturePath, modelPath, pos, raylib::RColor::RWHITE);
+}
+
+void Block::internalFactory(BlockType type, raylib::MyVector3 pos, raylib::MyVector3 size)
+{
+    try {
+        std::function<void (raylib::MyVector3 pos, raylib::MyVector3 size)> func = this->_factory.at(type);
+
+        if (func)
+            func(pos, size);
+    } catch (const std::out_of_range &e) {
+        (void) e;
+        return;
+    }
+}
+
+void Block::softFactory(raylib::MyVector3 pos, raylib::MyVector3 size)
+{
+    Game::CoreData::entityManager->addComponent<Component::Hitbox>(this->_entity, pos, size,
+        []([[maybe_unused]] const Engine::Entity &fromEntity, [[maybe_unused]] const Engine::Entity &toEntity) {
+            // TODO ADD CALLBACK UNDOMOVE
+    });
+}
+
+void Block::hardFactory(raylib::MyVector3 pos, raylib::MyVector3 size)
+{
+    Game::CoreData::entityManager->addComponent<Component::Hitbox>(this->_entity, pos, size,
+        []([[maybe_unused]] const Engine::Entity &fromEntity, [[maybe_unused]] const Engine::Entity &toEntity) {
+            // TODO ADD CALLBACK UNDOMOVE
+    });
+}
+
+void Block::bombFactory(raylib::MyVector3 pos, raylib::MyVector3 size)
+{
+    Game::CoreData::entityManager->addComponent<Component::Hitbox>(this->_entity, pos, size,
+        []([[maybe_unused]] const Engine::Entity &fromEntity, [[maybe_unused]] const Engine::Entity &toEntity) {
+            // TODO ADD CALLBACK UNDOMOVE
+    });
+}
+
+void Block::blastFactory(raylib::MyVector3 pos, raylib::MyVector3 size)
+{
+    Game::CoreData::entityManager->addComponent<Component::Hitbox>(this->_entity, pos, size,
+        []([[maybe_unused]] const Engine::Entity &fromEntity, [[maybe_unused]] const Engine::Entity &toEntity) {
+            // TODO ADD CALLBACK KILLENTITY
+    });
 }
--- src/GUI/Entities/Block/Block.hpp
+++ src/GUI/Entities/Block/Block.hpp
@@ -21,6 +21,8 @@ namespace Entities
         {
             BLOCK_HARD,
             BLOCK_SOFT,
+            BLOCK_BOMB,
+            BLOCK_BLAST,
             BLOCK_FLOOR,
         };
         public:
@@ -29,11 +31,27 @@ namespace Entities
 
         private:
             std::shared_ptr<raylib::Model> getModel(raylib::MyVector3 pos, BlockType type) const;
+            // FACTORY
+            void internalFactory(BlockType type, raylib::MyVector3 pos, raylib::MyVector3 size);
 
-            private:
-                BlockType _type;
-                std::string _name;
-                Engine::Entity _entity;
+            // FACTORY_FUNC
+            void softFactory(raylib::MyVector3 pos, raylib::MyVector3 size);
+            void hardFactory(raylib::MyVector3 pos, raylib::MyVector3 size);
+            void bombFactory(raylib::MyVector3 pos, raylib::MyVector3 size);
+            void blastFactory(raylib::MyVector3 pos, raylib::MyVector3 size);
+
+        private:
+            BlockType _type;
+            std::string _name;
+            Engine::Entity _entity;
+            raylib::MyVector3 _pos;
+            std::unordered_map<BlockType, std::function<void(raylib::MyVector3 pos, raylib::MyVector3 size)>> _factory = {
+                {BlockType::BLOCK_HARD, [this](raylib::MyVector3 pos, raylib::MyVector3 size) {this->hardFactory(pos, size);}},
+                {BlockType::BLOCK_SOFT, [this](raylib::MyVector3 pos, raylib::MyVector3 size) {this->softFactory(pos, size);}},
+                {BlockType::BLOCK_BOMB, [this](raylib::MyVector3 pos, raylib::MyVector3 size) {this->bombFactory(pos, size);}},
+                {BlockType::BLOCK_BLAST, [this](raylib::MyVector3 pos, raylib::MyVector3 size) {this->blastFactory(pos, size);}},
+                {BlockType::BLOCK_FLOOR, nullptr},
+            };
     };
 }
 
