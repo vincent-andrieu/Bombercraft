fd0f5ad3019adb8381a620c3dcc2d9e77fb452a1
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-06-09 14:51:42 +0200

    Fix merge

--- src/Game/Factories/ButtonFactory.hpp
+++ src/Game/Factories/ButtonFactory.hpp
@@ -47,12 +47,11 @@ namespace Game
         const auto &staticConf = *dynamic_cast<const ButtonStaticConfig *>(&staticData);
         const auto &dynConf = *dynamic_cast<const ButtonDynamicConfig *>(&dynamicData);
 
-        //        CoreData::entityManager->addComponent<Component::ClickEvent>(entity, dynConf.handler, dynConf.requirements);
-        //        // CoreData::entityManager->addComponent<Component::MouseMoveEvent>(entity, );
-        //        CoreData::entityManager->addComponent<Component::Render2D>(entity,
-        //            render2dMapModels{
-        //                {"background", std::make_shared<raylib::Texture>(staticConf.texturePath, dynConf.position)}
-        //            });
+        CoreData::entityManager->addComponent<Component::ClickEvent>(entity, dynConf.handler, staticConf.requirements);
+        CoreData::entityManager->addComponent<Component::MouseMoveEvent>(entity, dynConf.handler, staticConf.requirements);
+        CoreData::entityManager->addComponent<Component::Render2D>(entity,
+            Component::render2dMapModels{
+                {"background", std::make_shared<raylib::Texture>(staticConf.texturePath, dynConf.position)}});
         return entity;
     };
 } // namespace Game
