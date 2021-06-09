aa700ffa8ba379c6e1f7dba75478933eef3c4cd8
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-06-09 13:01:05 +0200

    - Add start of Block

---
+++ src/GUI/Entities/Block/Block.cpp
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2021
+** Block
+** File description:
+** Block.cpp
+*/
+
+#include "Block.hpp"
+
+using namespace Entities;
+
+Block::Block(Engine::EntityPack &entities, const std::string &name, raylib::MyVector3 pos, BlockType type)
+: _type(type), _entity(entities.createEntity(name))
+{
+    raylib::MyVector3 size = Game::CoreData::settings->getMyVector3("DEFAULT_BLOCK_SIZE");
+    std::shared_ptr<raylib::Model> model = this->getModel(pos, type);
+
+    Game::CoreData::entityManager->addComponent<Engine::Position>(this->_entity, pos.a, pos.b, pos.c);
+    Game::CoreData::entityManager->addComponent<Component::Render3D>(this->_entity, model);
+    if (type != BlockType::BLOCK_FLOOR) {
+        Game::CoreData::entityManager->addComponent<Component::Hitbox>(this->_entity, pos, size,
+            []([[maybe_unused]] const Engine::Entity &fromEntity, [[maybe_unused]] const Engine::Entity &toEntity) {
+        });
+    }
+}
+
+Block::~Block()
+{
+
+}
+
+std::shared_ptr<raylib::Model> Block::getModel(raylib::MyVector3 pos, BlockType type) const
+{
+    std::string modelPath = Game::CoreData::settings->getString("BLOCK_MODEL");
+    std::string texturePath;
+    std::string typeInStr;
+
+    if (type == BlockType::BLOCK_FLOOR)
+        typeInStr = "FLOOR";
+    else if (type == BlockType::BLOCK_SOFT)
+        typeInStr = "SOFT";
+    else if (type == BlockType::BLOCK_HARD)
+        typeInStr = "HARD";
+    else
+        throw std::invalid_argument("Not define in enum");
+    texturePath = Game::CoreData::settings->getString("BLOCK_" + typeInStr + "_TEXTURE");
+    return std::make_shared<raylib::Model>(texturePath, modelPath, pos, raylib::RColor::RWHITE);
+}
---
+++ src/GUI/Entities/Block/Block.hpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2021
+** indiestudio
+** File description:
+** Block.hpp
+*/
+
+#ifndef BLOCKENTITY_HPP_
+#define BLOCKENTITY_HPP_
+
+#include "GameEngine.hpp"
+#include "EntityPack/EntityPack.hpp"
+#include "Systems/Render2D/Render2DSystem.hpp"
+#include "Systems/Render3D/Render3DSystem.hpp"
+
+namespace Entities
+{
+    class Block
+    {
+        enum class BlockType
+        {
+            BLOCK_HARD,
+            BLOCK_SOFT,
+            BLOCK_FLOOR,
+        };
+        public:
+            Block(Engine::EntityPack &entities, const std::string &name, raylib::MyVector3 pos, BlockType type);
+            ~Block();
+
+        private:
+            std::shared_ptr<raylib::Model> getModel(raylib::MyVector3 pos, BlockType type) const;
+
+            private:
+                BlockType _type;
+                Engine::Entity _entity;
+    };
+}
+
+#endif /* !EXCEPTIONS_HPP_ */
