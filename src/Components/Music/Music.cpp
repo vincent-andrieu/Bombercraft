4ffc6ca9d8109e5f2bb17f3b7f34b1939775754c
RACAUD Simon <simon.racaud@epitech.eu>
2021-06-10 20:35:44 +0200

    feat(core): music component

---
+++ src/Components/Music/Music.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** gameEngine
+** File description:
+** 10/06/2021 Music.cpp.cc
+*/
+
+#include "Music.hpp"
+
+Component::Music::Music(std::shared_ptr<raylib::Music> music) : sound(music)
+{
+}
