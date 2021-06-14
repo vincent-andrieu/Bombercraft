d6a80aec6c34a1d85f775ec16f6b68e3a34cc534
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-06-14 09:41:17 +0200

    fix: compatibilitie, init structure without .varname

--- src/GUI/Factories/KeyInput/KeyInputFactory.cpp
+++ src/GUI/Factories/KeyInput/KeyInputFactory.cpp
@@ -264,10 +264,10 @@ void KeyInputFactory::create(Engine::EntityPack &pack, KeyInputDynConf const &dy
 
 KeyInputConfig KeyInputFactory::getStandardConfig()
 {
-    const KeyInputConfig t = {.size = raylib::MyVector2(152, 27),
-        .color = raylib::RColor::RBLACK,
-        .borderSize = 2,
-        .borderColor = raylib::RColor::RGRAY,
-        .textPositionOffset = raylib::MyVector2(5, 5)};
+    const KeyInputConfig t = {raylib::MyVector2(152, 27),
+        raylib::RColor::RBLACK,
+        2,
+        raylib::RColor::RGRAY,
+        raylib::MyVector2(5, 5)};
     return t;
 }
