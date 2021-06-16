068b856aab51be07c48e70000afc95226dc724dd
RACAUD Simon <simon.racaud@epitech.eu>
2021-06-16 16:11:17 +0200

    chore(core): in hitbox component, add debug function. display the hitbox

--- src/Components/Hitbox/Hitbox.cpp
+++ src/Components/Hitbox/Hitbox.cpp
@@ -6,17 +6,29 @@
  */
 
 #include "Hitbox.hpp"
+#include "Game/CoreData/CoreData.hpp"
 
 using namespace Component;
 
 Hitbox::Hitbox(const raylib::MyVector3 &origin, const raylib::MyVector3 &size, hitboxHandler handler, Game::EntityType typeEntity)
-    : objectBox(std::make_shared<ObjectBox>(origin, size)), entityType(typeEntity), prevPosition(0, 0, 0), _handler(handler)
+    : objectBox(std::make_shared<ObjectBox>(origin, size)), entityType(typeEntity), _handler(handler), _debugRender(nullptr, origin, size)
 {
 }
 
 void Hitbox::trigger(const Engine::Entity &fromEntity, const Engine::Entity &toEntity)
 {
     this->_handler(fromEntity, toEntity);
+}
+
+void Hitbox::debugRender()
+{
+    raylib::MyVector3 position = this->objectBox->getBoxOrigin();
+    raylib::MyVector3 size = this->objectBox->getBoxSize();
+    this->_debugRender.setPosition(position);
+    this->_debugRender.setSize(size);
+    Game::CoreData::camera->begin3D();
+    this->_debugRender.draw();
+    Game::CoreData::camera->end3D();
 }
 
 // bool Hitbox::save(Engine::SaveManager &saver) const
