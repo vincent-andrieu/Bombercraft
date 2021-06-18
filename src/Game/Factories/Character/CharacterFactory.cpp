a9561c4b1d2012ec15b9500fa712c033ca49233d
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-06-18 14:54:08 +0200

    - Add velocity in option

--- src/Game/Factories/Character/CharacterFactory.cpp
+++ src/Game/Factories/Character/CharacterFactory.cpp
@@ -251,7 +251,7 @@ void CharacterFactory::handlerAITimer(
     Engine::Entity entityPlayer;
     auto &map = CoreData::entityManager->getComponent<Component::Matrix2D>(
         sceneManager.getCurrentScene()->localEntities.getEntity("gameMap"));
-    // auto &velocity = CoreData::entityManager->getComponent<Engine::Velocity>(entity);
+    auto &velocity = CoreData::entityManager->getComponent<Engine::Velocity>(entity);
     auto &ai = CoreData::entityManager->getComponent<Component::AIComponent>(entity);
     Component::ModelList &render = CoreData::entityManager->getComponent<Component::ModelList>(entity);
 
@@ -282,18 +282,15 @@ void CharacterFactory::handlerAITimer(
     }
     ai.setEnv(map.getData(), {(size_t) relativPos.a, (size_t) relativPos.b}, posList);
     std::pair<double, double> velocityIA = ai.getVelocity();
-    // velocity.x = (float) velocityIA.first;
-    // velocity.y = (float) velocityIA.second;
-
-    const raylib::MyVector3 &position =
+    
+    if (Game::CoreData::settings->getInt("AI_VELOCITY_MODE") == 1) {
+        velocity.x = (float) velocityIA.first;
+        velocity.y = (float) velocityIA.second;
+    } else {
+        const raylib::MyVector3 &position =
         render.getPosition() + raylib::MyVector3(static_cast<float>(velocityIA.first), 0, static_cast<float>(velocityIA.second));
-    render.setPosition(position);
-
-    // auto pos = hitbox.objectBox->getBoxOrigin();
-
-    hitbox.objectBox->setOrigin(
-        raylib::MyVector3({position.a, position.b, position.c}) + Game::CoreData::settings->getMyVector3("AI_SHIFT"));
-
-    // auto size = hitbox.objectBox->getBoxSize();
-    // hitbox.objectBox->setOrigin({pos.a + velocityIA.first * size.a, pos.b + velocityIA.second * size.b, pos.c + 0 * size.c});
+        render.setPosition(position);
+        hitbox.objectBox->setOrigin(
+            raylib::MyVector3({position.a, position.b, position.c}) + Game::CoreData::settings->getMyVector3("AI_SHIFT"));
+    }
 }
