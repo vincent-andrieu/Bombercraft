0bdf2f3904113024d4497fed73ba0d4f80e9342e
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-06-19 15:48:39 +0200

    Add button with picture

--- src/GUI/Factories/Button/ButtonFactory.cpp
+++ src/GUI/Factories/Button/ButtonFactory.cpp
@@ -71,40 +71,40 @@ Engine::Entity GUI::ButtonFactory::create(Engine::EntityPack &pack,
     const Component::eventScript clickAction,
     const bool centered)
 {
-    const auto &my_size(conf.size);
-    auto my_position((position));
+    const auto &mySize(conf.size);
+    auto myPosition((position));
     Engine::Entity entity;
     if (centered)
-        my_position = my_position - ProportionUtilities::getProportionWin(my_size, raylib::MyVector2(50, 50));
+        myPosition = myPosition - ProportionUtilities::getProportionWin(mySize, raylib::MyVector2(50, 50));
     if (name.empty()) {
         throw std::invalid_argument("ButtonFactory::create empty entity name");
     } else {
         entity = pack.createEntity(name);
     }
 
-    auto my_label(std::make_shared<raylib::Text>(label,
-        my_position,
+    auto myLabel(std::make_shared<raylib::Text>(label,
+        myPosition,
         conf.fontSize,
         conf.fontColor,
         std::shared_ptr<raylib::Font>(std::make_shared<raylib::Font>(conf.fontPath))));
 
-    raylib::Text::setFontSize(*my_label, (conf.size.a < 20 && conf.size.b < 20) ? conf.size : conf.size - 20);
-    auto my_labelPosition(my_position
+    raylib::Text::setFontSize(*myLabel, (conf.size.a < 20 && conf.size.b < 20) ? conf.size : conf.size - 20);
+    auto myLabelPosition(myPosition
         + ProportionUtilities::getProportionWin(
-            my_size, raylib::MyVector2(50, 50), my_label->getSize(), raylib::MyVector2(50, 50)));
-    my_label->setPosition(my_labelPosition);
+            mySize, raylib::MyVector2(50, 40), myLabel->getSize(), raylib::MyVector2(50, 40)));
+    myLabel->setPosition(myLabelPosition);
     Component::render2dMapModels my_models({
-        {"unavailable", std::make_shared<raylib::Texture>(conf.unavailableTexturePath, my_size, my_position)},
-        {"idle", std::make_shared<raylib::Texture>(conf.idleTexturePath, my_size, my_position)},
-        {"hover", std::make_shared<raylib::Texture>(conf.hoverTexturePath, my_size, my_position)},
-        {"label", my_label},
+        {"unavailable", std::make_shared<raylib::Texture>(conf.unavailableTexturePath, mySize, myPosition)},
+        {"idle", std::make_shared<raylib::Texture>(conf.idleTexturePath, mySize, myPosition)},
+        {"hover", std::make_shared<raylib::Texture>(conf.hoverTexturePath, mySize, myPosition)},
+        {"label", myLabel},
     });
 
-    Component::eventScript my_moveHandler = [my_position, my_size](const Engine::Entity entity) {
+    Component::eventScript my_moveHandler = [myPosition, mySize](const Engine::Entity entity) {
         auto &my_render(Game::CoreData::entityManager->getComponent<Component::Render2D>(entity));
 
         if (!my_render.isSetToDraw("unavailable")) {
-            if (Game::CoreData::eventManager->MouseIsOver(my_position, my_size)) {
+            if (Game::CoreData::eventManager->MouseIsOver(myPosition, mySize)) {
                 my_render.setToDrawFirst("hover");
                 my_render.unsetToDraw("idle");
             } else {
@@ -113,10 +113,10 @@ Engine::Entity GUI::ButtonFactory::create(Engine::EntityPack &pack,
             }
         }
     };
-    Component::eventScript my_clickHandler = [my_position, my_size, clickAction](const Engine::Entity entity) {
+    Component::eventScript my_clickHandler = [myPosition, mySize, clickAction](const Engine::Entity entity) {
         const auto &my_render(Game::CoreData::entityManager->getComponent<Component::Render2D>(entity));
 
-        if (Game::CoreData::eventManager->MouseIsOverClicked(my_position, my_size) && !my_render.isSetToDraw("unavailable")) {
+        if (Game::CoreData::eventManager->MouseIsOverClicked(myPosition, mySize) && !my_render.isSetToDraw("unavailable")) {
             Game::CoreData::systemManager->getSystem<System::AudioSystem>().play("ButtonClick", core->globalEntities);
 
             clickAction(entity);
@@ -125,6 +125,77 @@ Engine::Entity GUI::ButtonFactory::create(Engine::EntityPack &pack,
     Game::CoreData::entityManager->addComponent<Component::ClickEvent>(entity, my_clickHandler, conf.requirements);
     Game::CoreData::entityManager->addComponent<Component::MouseMoveEvent>(entity, my_moveHandler);
     Game::CoreData::entityManager->addComponent<Component::Render2D>(entity, my_models);
+
+    auto &my_render(Game::CoreData::entityManager->getComponent<Component::Render2D>(entity));
+
+    my_render.unsetToDraw("hover");
+    my_render.unsetToDraw("unavailable");
+
+    return entity;
+}
+
+Engine::Entity ButtonFactory::create(Engine::EntityPack &pack,
+    const raylib::MyVector2 &position,
+    ButtonConfig const &conf,
+    const Component::eventScript clickAction,
+    const string &screenPath,
+    const string &label,
+    bool centered,
+    const string &name)
+{
+    auto mySize(conf.size);
+    auto myPosition(centered ? position - ProportionUtilities::getProportionWin(mySize, raylib::MyVector2(50, 50)) : position);
+    const Engine::Entity &entity = name.empty() ? pack.createAnonymousEntity() : pack.createEntity(name);
+    const raylib::MyVector2 screenSize((mySize.a * 20) / 100, mySize.b);
+
+    std::cout << "screenSize: " << screenSize << std::endl;
+    // Label
+    const raylib::MyVector2 labelSize(mySize.a, mySize.b);
+    auto myLabel(std::make_shared<raylib::Text>(label,
+        myPosition,
+        conf.fontSize,
+        conf.fontColor,
+        std::shared_ptr<raylib::Font>(std::make_shared<raylib::Font>(conf.fontPath))));
+    raylib::Text::setFontSize(*myLabel, (labelSize.a < 20 && labelSize.b < 20) ? labelSize : labelSize - 20);
+    myLabel->setPosition(myPosition + raylib::MyVector2(screenSize.a, 0)
+        + ProportionUtilities::getProportionWin(
+            labelSize, raylib::MyVector2(7, 35), myLabel->getSize(), raylib::MyVector2(7, 35)));
+
+    Component::render2dMapModels my_models({
+        {"unavailable", std::make_shared<raylib::Texture>(conf.unavailableTexturePath, mySize, myPosition)},
+        {"idle", std::make_shared<raylib::Texture>(conf.idleTexturePath, mySize, myPosition)},
+        {"hover", std::make_shared<raylib::Texture>(conf.hoverTexturePath, mySize, myPosition)},
+        {"label", myLabel},
+    });
+
+    // Button
+    Component::eventScript my_moveHandler = [myPosition, mySize](const Engine::Entity entity) {
+        auto &my_render(Game::CoreData::entityManager->getComponent<Component::Render2D>(entity));
+
+        if (!my_render.isSetToDraw("unavailable")) {
+            if (Game::CoreData::eventManager->MouseIsOver(myPosition, mySize)) {
+                my_render.setToDrawFirst("hover");
+                my_render.unsetToDraw("idle");
+            } else {
+                my_render.unsetToDraw("hover");
+                my_render.setToDrawFirst("idle");
+            }
+        }
+    };
+    Component::eventScript my_clickHandler = [myPosition, mySize, clickAction](const Engine::Entity entity) {
+        const auto &my_render(Game::CoreData::entityManager->getComponent<Component::Render2D>(entity));
+
+        if (Game::CoreData::eventManager->MouseIsOverClicked(myPosition, mySize) && !my_render.isSetToDraw("unavailable")) {
+            Game::CoreData::systemManager->getSystem<System::AudioSystem>().play("ButtonClick", core->globalEntities);
+
+            clickAction(entity);
+        }
+    };
+    Game::CoreData::entityManager->addComponent<Component::ClickEvent>(entity, my_clickHandler, conf.requirements);
+    Game::CoreData::entityManager->addComponent<Component::MouseMoveEvent>(entity, my_moveHandler);
+    Game::CoreData::entityManager->addComponent<Component::Render2D>(entity, my_models);
+    const raylib::MyVector2 buttonBorder((mySize.a * 0.6) / 100, (mySize.b * 5) / 100);
+    GUI::ImageFactory::create(pack, myPosition + buttonBorder, screenSize - buttonBorder * 2, screenPath, true);
 
     auto &my_render(Game::CoreData::entityManager->getComponent<Component::Render2D>(entity));
 
